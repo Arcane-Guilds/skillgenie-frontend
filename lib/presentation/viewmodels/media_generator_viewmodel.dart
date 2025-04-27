9dd1a483fdf7d2194620d5e2ec4370c50b21619c
Mega Pc <yassine.khraief@esprit.tn>
2025-04-27 22:43:00 +0100

    video

--- lib/presentation/viewmodels/media_generator_viewmodel.dart
+++ lib/presentation/viewmodels/media_generator_viewmodel.dart
@@ -66,13 +66,13 @@ class MediaGeneratorViewModel extends ChangeNotifier {
     final textPainter = TextPainter(
       text: TextSpan(
         text: text,
-        style: const TextStyle(color: Colors.black, fontSize: 24),
+        style: TextStyle(color: Colors.black, fontSize: 24),
       ),
       textDirection: TextDirection.ltr,
     );
 
     textPainter.layout(maxWidth: 800);
-    textPainter.paint(canvas, const Offset(50, 50));
+    textPainter.paint(canvas, Offset(50, 50));
 
     final picture = recorder.endRecording();
     final img = await picture.toImage(900, 600);

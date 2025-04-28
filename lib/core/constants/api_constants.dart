43de438b18ce5a1bfa827049b26084a53de2ca1c
nada.daadi@esprit.tn <nada.daadi@esprit.tn>
2025-04-28 10:06:39 +0100

    init

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
 
-  static String get baseUrl => dotenv.env['API_BASE_URL'] ?? 'https://9ea4-41-225-249-1.ngrok-free.app';
+  static String get baseUrl => dotenv.env['API_BASE_URL'] ?? 'https://10.0.2.2:3000';
 
 }
 

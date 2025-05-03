5f57db99cab51e69b6cc93fc57d7839a57cdc5f3
Mega Pc <yassine.khraief@esprit.tn>
2025-05-03 18:31:19 +0100

    .

--- lib/presentation/viewmodels/lesson_view_model.dart
+++ lib/presentation/viewmodels/lesson_view_model.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
 
-import 'package:ffmpeg_kit_flutter_new/return_code.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
-import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit.dart';
-
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter/return_code.dart';
 
 import 'package:path_provider/path_provider.dart';
 import 'package:gal/gal.dart';
